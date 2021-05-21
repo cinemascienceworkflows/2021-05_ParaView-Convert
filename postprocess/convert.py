a7ead1f5fe635cd84b52d15067fe221e98f6f843
David Rogers <dhr@lanl.gov>
2021-05-21 04:37:40 -0400

    updating conversion

--- postprocess/convert.py
+++ postprocess/convert.py
@@ -19,9 +19,12 @@ if (len(sys.argv) == 3):
 else:
     print("ERROR: must provide indir and outdir")
 
+print(indir)
+print(outdir)
 os.mkdir(os.path.join(outdir, cdbbase))
 
 reader = vtkStructuredPointsReader()
+dims = []
 for f in glob.glob("{}/*.vtk".format(indir)):
     reader.SetFileName(f)
     reader.ReadAllVectorsOn()
@@ -30,17 +33,17 @@ for f in glob.glob("{}/*.vtk".format(indir)):
 
     data = reader.GetOutput()
 
-    dim = data.GetDimensions()
-    vec = list(dim)
-    vec = [i-1 for i in dim]
-    vec.append(3)
+    dims = data.GetDimensions()
 
     u = VN.vtk_to_numpy(data.GetPointData().GetArray('scalars'))
+    unewshape = numpy.reshape(u, (dims[1], dims[0]))
+    unewtrans = unewshape.transpose()
+    unew      = numpy.fliplr(unewtrans)
     basename = os.path.basename(f)
-    basename = basename.split(".")[0]
+    basename = re.sub('\.vtk$', '', basename)
     fullpath = os.path.join(outdir, cdbbase, basename + ".npz")
     print(fullpath)
-    numpy.savez_compressed(fullpath, data=u)
+    numpy.savez_compressed(fullpath, data=unew)
 
 # update the data.csv file
 oldcsv = os.path.join(indir, "data.csv")
@@ -49,9 +52,18 @@ newcsv = os.path.join(outdir, cdbbase, "data.csv")
 print(oldcsv)
 print(newcsv)
 with open(oldcsv, 'r') as ofile, open(newcsv, 'w') as nfile:
+    first = ofile.readline()
+    clean = first.strip()
+    newline = re.sub('\_vtk$', '', clean)
+    nfile.write(newline)
+    nfile.write(",CISVersion,CISImage,CISImageWidth,CISImageHeight,CISLayer,CISLayerWidth,CISLayerHeight,CISLayerOffsetX,CISLayerOffsetY,CISChannel,CISChannelVar")
+    nfile.write("\n")
+
     lines = ofile.readlines()
+    curimage = 0
     for l in lines:
         clean = l.strip()
         newline = re.sub('\.vtk$', '.npz', clean)
         nfile.write(newline)
-        nfile.write("\n")
+        nfile.write(",\"1.0\",i{},{},{},l000,{},{},0,0,scalars,scalars\n".format(f'{curimage:03}', dims[0], dims[1], dims[0], dims[1]))
+        curimage = curimage + 1
